c21d9adbda785097a53cc9cedd3610edfd7b3a89
AndreyWeber <andrey.weber@gmail.com>
2024-09-15 17:56:41 +0100

    Added AG Grid

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import { AgGridVue } from 'ag-grid-vue';
 
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+// Import AG Grid theme CSS
+import 'ag-grid-community/dist/styles/ag-grid.css';
+// You can choose a different theme
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 Vue.config.productionTip = false;
 
@@ -14,6 +19,8 @@ Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
+
+Vue.component("AgGridVue", AgGridVue);
 
 new Vue({
   router,
